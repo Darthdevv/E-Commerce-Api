b82abd4c180419606b421fba670f9923a2c3874c
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-08-16 17:29:39 +0300

    🛒 feat: adding category model

---
+++ models/category.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false, // TODO: Change to true after adding authentication
+    },
+    Images: {
+      secure_url: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const Category = model("Category", categorySchema);
