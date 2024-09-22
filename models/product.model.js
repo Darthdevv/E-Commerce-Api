f4f249b0ed95fcc37546ca6bc844d6daee6b4332
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-09-23 01:27:21 +0300

    🛒 feat: products model

--- models/product.model.js
+++ models/product.model.js
@@ -4,24 +4,95 @@ const { Schema, model } = mongoose;
 const productSchema = new Schema(
   {
     // Strings
-    title: {},
-    slug: {},
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
     overview: String,
     specifications: Object,
-    badge: {},
+    badge: {
+      type: String,
+      enum: ["New", "Sale", "Best Seller"],
+    },
     // Numbers
-    price: {},
-    discount: {},
-    priceAfterDiscount: {},
-    stock: {},
-    rating: {},
+    price: {
+      type: Number,
+      required: true,
+      min: 50,
+    },
+    discount: {
+      type: {
+        type: String,
+        enum: ["Percentage", "Fixed"],
+        default: "Percentage",
+      },
+      amount: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+    priceAfterDiscount: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 10,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
     // Images
-    images: {},
+    images: {
+      URLs: {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+        public_id: {
+          type: String,
+          required: true,
+          unique: true,
+        }
+      },
+      customId: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+    },
     // Ids
-    categoryId: {},
-    subCategoryId: {},
-    brandId: {},
-    createdBy: {},
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    subCategoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: true,
+    },
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false, // TODO: Change to true after adding authentication
+    },
   },
   { timestamps: true }
 );
