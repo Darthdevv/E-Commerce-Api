d5a78830699545e75e7a2a1a04d71577000ee44a
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-09-24 02:45:03 +0300

    🛒 feat: products controllers

---
+++ controllers/product.controllers.js
@@ -0,0 +1,4 @@
+import { Brand } from "../models/brand.model";
+import { Category } from "../models/category.model";
+import { SubCategory } from "../models/subcategory.model";
+import { Product } from "../models/product.model";
